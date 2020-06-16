0418b65959ca056f5eefb26986765ad6736c62da
Adam Mathieson <mathieson1222@gmail.com>
2020-06-15 19:12:47 -0700

    changes the fade in animation on the menu tabs using index in a forEach loop; adds a burger menu for the onclick

--- js/frosted-glass.js
+++ js/frosted-glass.js
@@ -4,12 +4,23 @@ export const menuOpen = () => {
 }
 
 export const tabsAnimation = () => {
+    const menu = document.querySelector('.menu-right')
+    const burger = document.querySelector('.burger')
     const tabs = document.querySelectorAll('.menu-right nav a')
-    let duration = 1
+    
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('menu-active')
+        tabs.forEach((tab, index) => {
+            if (tab.style.animation) {
+                tab.style.animation = ''
+            } else {
+                tab.style.animation = `tabs-fade-in 1.5s ease forwards ${index/7 + 0.3}s`
+            }
+            // console.log(index/7)
+            // console.log(tab.style.animation)
+        })
+    })
 
-    for (let i = 0; i < tabs.length; i++) {
-        tabs[i].style.animation = `fade-in ${duration}s`
-        duration += 1
-    }
+    
 
 }
