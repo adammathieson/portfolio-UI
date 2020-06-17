8bb7dcb6ed872c15d90deeb13cb396ff2b312ff1
Adam Mathieson <mathieson1222@gmail.com>
2020-06-17 00:43:13 -0700

    postions and replaces the click eventlisner to svg arrow icon

--- js/frosted-glass.js
+++ js/frosted-glass.js
@@ -5,16 +5,16 @@ export const menuOpen = () => {
 
 export const tabsAnimation = () => {
     const menu = document.querySelector('.menu-right')
-    const burger = document.querySelector('.burger')
+    const arrow = document.querySelector('#arrow')
     const tabs = document.querySelectorAll('.menu-right nav a')
     
-    burger.addEventListener('click', () => {
+    arrow.addEventListener('click', () => {
         menu.classList.toggle('menu-active')
         tabs.forEach((tab, index) => {
             if (tab.style.animation) {
                 tab.style.animation = ''
             } else {
-                tab.style.animation = `tabs-fade-in 1.5s ease forwards ${index/7 + 0.3}s`
+                tab.style.animation = `tabs-fade-in 0.5s ease forwards ${index/7 + 0.3}s`
             }
             // console.log(index/7)
             // console.log(tab.style.animation)
