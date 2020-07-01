1dc9687731a4bb507fc22cb481c87db0e9a8229d
Adam Mathieson <mathieson1222@gmail.com>
2020-07-01 14:12:57 -0700

    adds front feet animation

--- js/forms/registration.js
+++ js/forms/registration.js
@@ -7,5 +7,9 @@ const tl = gsap.timeline({repeat: 3, repeatDelay: .1, yoyo: true})
     tl.to("#rEye", {r: 0, duration: .1})
     tl.fromTo("#nose", {rotate: -10}, {rotate: 10, duration: .1})
 
+    tl.fromTo("#left-foot", {y: 0}, {y: -2, duration: .2})
+    tl.fromTo("#right-foot", {y: 0}, {y: -2, duration: .2}, .5)
+
+
 // gsap.timeline().from("#rEye", {r: 0})
 console.log('forms!!!')
