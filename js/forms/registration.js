976d260ac7f93aaae3c02519a21d33b38dfef1d1
Adam Mathieson <mathieson1222@gmail.com>
2020-07-01 14:07:42 -0700

    adds animations to weasel eyes and nose; replaces weasel with a filled version to animate the feet

--- js/forms/registration.js
+++ js/forms/registration.js
@@ -1,8 +1,11 @@
 const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")
 
 
+const tl = gsap.timeline({repeat: 3, repeatDelay: .1, yoyo: true})
+    tl.set("#nose", {transformOrigin: "50% 50%"})
+    tl.to("#lEye", {rx: 0, duration: .1})
+    tl.to("#rEye", {r: 0, duration: .1})
+    tl.fromTo("#nose", {rotate: -10}, {rotate: 10, duration: .1})
 
-
-
-
+// gsap.timeline().from("#rEye", {r: 0})
 console.log('forms!!!')
