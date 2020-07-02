0b2c703c983e277ebb2d8ecbb94b449312f8d285
Adam Mathieson <mathieson1222@gmail.com>
2020-07-01 17:08:18 -0700

    adds svg title for animating

--- js/forms/registration.js
+++ js/forms/registration.js
@@ -7,8 +7,8 @@ const tl = gsap.timeline({repeat: 3, repeatDelay: .1, yoyo: true})
     tl.to("#rEye", {r: 0, duration: .1})
     tl.fromTo("#nose", {rotate: -10}, {rotate: 10, duration: .1})
 
-    tl.fromTo("#left-foot", {y: 0}, {y: -2, duration: .2})
-    tl.fromTo("#right-foot", {y: 0}, {y: -2, duration: .2}, .5)
+    tl.fromTo("#left-foot", {y: 0}, {y: -1.2, duration: .2})
+    tl.fromTo("#right-foot", {y: 0}, {y: -1.3, duration: .2}, .5)
 
 
 // gsap.timeline().from("#rEye", {r: 0})
