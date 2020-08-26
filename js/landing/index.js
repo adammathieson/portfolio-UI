4058bb980af3f47fcd99adac95430e8995e7fcf9
Adam Mathieson <mathieson1222@gmail.com>
2020-08-26 14:05:45 -0700

    adds an img tag to the svg to be revealed in animation, scales and responsive

--- js/landing/index.js
+++ js/landing/index.js
@@ -2,7 +2,7 @@
 
 // var fullBg = document.querySelector("#")
 
-var curvedBg = "M0 0.00146484H1059.52C1059.52 0.00146484 1165.78 165.079 838.55 491.373C580.049 749.135 716.797 926.689 716.797 926.689H0V0.00146484Z"
+var curvedBg = "M0 0H1086.91C1086.91 0 1261.61 178.138 907.481 530.245C627.731 808.399 775.72 1000 775.72 1000H0V0Z"
 
 var landingMorph = anime({
     targets: '.landing-bg',
@@ -11,7 +11,7 @@ var landingMorph = anime({
       }],
     easing: 'easeOutQuad',
     direction: 'alternate',
-    duration: 2000,
+    duration: 10000,
     loop: true,
 
 
