f190077663307838647be946ccb8119c43845285
Adam Mathieson <mathieson1222@gmail.com>
2020-08-30 17:16:50 -0700

    adds tablet width view below 620px with; adds mobile breakpoint and positions the navbar

--- js/landing/index.js
+++ js/landing/index.js
@@ -6,7 +6,11 @@ var developerChars = document.querySelectorAll('#developer path')
 
 var adam = document.querySelector('#adam')
 
+var deviceWidth = window.innerWidth
+var tabletWidth = 620
+console.log(deviceWidth)
 var curvedBg = "M0 0H1086.91C1086.91 0 1261.61 178.138 907.481 530.245C627.731 808.399 775.72 1000 775.72 1000H0V0Z"
+var curvedBgMobile = "M0 0H824.265C824.265 0 1228.7 207.512 824.265 540.025C485.813 818.285 1121 1000 1121 1000H0V0Z"
 
 // console.log(adamChars)
 // console.log(mathiesonChars)
@@ -41,7 +45,7 @@ tl.add({
 .add({
     targets: '#landing-bg-full',
     d: [
-        { value: curvedBg
+        { value: deviceWidth < tabletWidth ? curvedBgMobile : curvedBg
         }],
     easing: 'easeOutQuad',
     direction: 'alternate',
