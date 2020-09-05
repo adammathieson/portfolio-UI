346783f21d6c0c09d4e9e8437e84115086e8b05f
Adam Mathieson <mathieson1222@gmail.com>
2020-09-05 13:27:40 -0700

    adds styling/positioning of elements — fine tuning; adds a projects.js file to render the projects as objects in one container

---
+++ js/projects/projects.js
@@ -0,0 +1,10 @@
+export const wmp = {
+    id : 'wmp',
+    image : [
+        '/Users/Administrator/UI/portfolio-UI/image/projects/wmp/wmp-plant-lib.jpg',
+        '/Users/Administrator/UI/portfolio-UI/image/projects/wmp/wmp-log-in.jpg',
+        '/Users/Administrator/UI/portfolio-UI/image/projects/wmp/wmp-add-plant.jpg',
+    ],
+    description: 'A scheduling and notification app to assist users on when and how to care for their plants. Worked as a front-end developer on this one week build while maintaining thorough remote communication with a collaborative five person team including UI, back end, additional front end engineer, and team leader.',
+    icons: ['react-logo', 'redux-logo', 'node-logo', 'material-ui-logo', 'moment-logo']
+}
