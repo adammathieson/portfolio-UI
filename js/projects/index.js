0734a31c9002de68cb55662bad3897b25b476e68
Adam Mathieson <mathieson1222@gmail.com>
2020-09-14 13:29:53 -0700

    removes old images from contianer berfore loading the next project image(s)

--- js/projects/index.js
+++ js/projects/index.js
@@ -1,59 +1,74 @@
 import {projects} from './projects.js'
 import {animations} from './animation.js'
     
-
-    const projectLoader = (projectArr) => {
-        let current = 0
-        let project = projectArr[current]
-        // console.log(project)
-        
-        const logos = window.document.querySelectorAll('.tech-logo div')
-        const description = window.document.querySelector('#description')
-        const images = window.document.querySelector('.image-container')
-        const titles = window.document.querySelectorAll('.title-container div')
-        const nextBtn = window.document.querySelector('.next-control button')
-
-        
-        description.textContent = project.description
-
-        logos.forEach(logo => {
-            if (project.icons.includes(logo.id)) {
-                logo.style.display = 'block'
-            }
-        })
-
-        // to set image container width
-        let imgWidth = Math.floor((1 / project.images.length) * 100)
-
-        project.images.forEach(url => {
-            const img = document.createElement('img')
-            img.src = url
-            img.style.width = String(imgWidth) + '%'
-            images.appendChild(img)
-        })
-
-        // set title
-        titles.forEach(title => {
-            if (title.id === `${project.id}-title`) {
-                title.style.display = 'block'
-            }
-        })
-
-        // Select a project
-
-        nextBtn.addEventListener('click', () => {
-            if (current === projectArr.length -1) {
-                current = 0
-            } else {
-                current++
-                projectLoader(projects)
-                console.log(current)
-            }
-        })
-
+let current = 0 
+const projectLoader = () => {
+    let project = projects[current]
+    // console.log(project)
+    
+    const logos = window.document.querySelectorAll('.tech-logo div')
+    const description = window.document.querySelector('#description')
+    const images = window.document.querySelector('.image-container')
+    const titles = window.document.querySelectorAll('.main-title')
+    // console.log(images.children)
 
     
-}
+    description.textContent = project.description
 
-projectLoader(projects)
+    logos.forEach(logo => {
+        if (project.icons.includes(logo.id)) {
+            logo.style.display = 'block'
+        }
+    })
+
+
+    // Clear out any images in container
+    if(images.children.length > 0) {
+        console.log(images.children.length)
+        while(images.firstChild) {
+            images.removeChild(images.firstChild)
+        }
+    }
+
+    // to set image container width
+    let imgWidth = Math.floor((1 / project.images.length) * 100)
+
+    project.images.forEach(url => {
+        const img = document.createElement('img')
+        img.src = url
+        img.style.width = String(imgWidth) + '%'
+        images.appendChild(img)
+    })
+
+    // set title
+    titles.forEach(title => {
+        if (title.id === `${project.id}-title`) {
+            title.style.display = 'block'
+            } else {
+                    title.style.display = 'none'
+                }
+                // console.log(title)
+    })
+
+    
+    
+    
+}
+    
+// Select a project
+
+const nextBtn = window.document.querySelector('.next-control button')
+
+nextBtn.addEventListener('click', () => {
+    if (current === projects.length -1) {
+        current = 0
+        // projectLoader()
+    } else {
+        current++
+        // console.log(current)
+    }
+    projectLoader()
+
+})
+// projectLoader(projects)
 animations()
