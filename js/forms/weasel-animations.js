26f5d400544760ebf9a13d54e6236a56580250b3
Adam Mathieson <mathieson1222@gmail.com>
2020-09-21 23:41:14 -0700

    weasel scratch position

--- js/forms/weasel-animations.js
+++ js/forms/weasel-animations.js
@@ -1,12 +1,19 @@
 
 const weaselForwardFace = window.document.querySelector("#weasel-forward-face")
 const weaselAltFace = window.document.querySelector("#weasel-alt-face")
-const weaselScratch = window.document.querySelector("#weasel-scratch")
+const weaselScratching = window.document.querySelector("#weasel-scratch")
 
 export function weaselLookBack() {
     weaselAltFace.style.visibility = 'visible'
     weaselForwardFace.style.visibility = 'hidden'
-    weaselScratch.style.visibility = 'hidden'
+    weaselScratching.style.visibility = 'hidden'
 }
 
-// weaselLookBack()
+export function weaselScratch() {
+    weaselAltFace.style.visibility = 'hidden'
+    weaselForwardFace.style.visibility = 'hidden'
+    weaselScratching.style.visibility = 'visible'
+}
+
+weaselLookBack()
+// weaselScratch()
