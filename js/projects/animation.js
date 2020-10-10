4c8dea1bf3000bc99db3902b807ff7fe92e31a27
Adam Mathieson <mathieson1222@gmail.com>
2020-10-09 17:40:13 -0700

    svg logo annimation added

--- js/projects/animation.js
+++ js/projects/animation.js
@@ -264,6 +264,22 @@ export function animations() {
             translateY: [5, 0],
         }, '-=1500')
 
+    var tlSvgLogo = anime.timeline({
+    })
+    .add({
+        targets: '#svg-vector',
+        rotate: [720, 0],
+        duration: 3000,
+        easing: 'linear',
+    })
+    .add({
+        targets: ['#S-svg', '#V-svg', '#G-svg'],
+        translateY: [70, 0],
+        duration: 1800,
+        easing: 'linear',
+        delay: anime.stagger(600),
+    }, '-=3000')
+
 }
 
 
