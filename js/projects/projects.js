fc7c0fe3bfee6ade52427a31ab2102f66daa5b26
Adam Mathieson <mathieson1222@gmail.com>
2020-10-09 17:23:48 -0700

    green sock animtation added

--- js/projects/projects.js
+++ js/projects/projects.js
@@ -8,8 +8,8 @@ export const projects = [
             
         ],
         description: 'Looking to take on more responsibility? Water My Plants makes planning and caring for your home and office flora a breeze. This application uses Node backend support to send watering and care instruction reminders for all types of plants. A gorgeous user interface and image selection API will get your home screen looking like a page out of Home and Garden. Create healthy habits and you\'ll be ready for a dog in no time!',
-        // icons: ['knex-logo'],
-        icons: ['react-logo', 'redux-logo', 'nodejs-logo', 'material-ui-logo', 'moment-logo', 'knex-logo'],
+        // icons: ['green-sock-logo'],
+        icons: ['react-logo', 'redux-logo', 'nodejs-logo', 'material-ui-logo', 'moment-logo'],
     },
     {
         id: 'endrsd',   
@@ -20,7 +20,7 @@ export const projects = [
     {
         id: 'home-devices',
         images: ['https://media.giphy.com/media/f4gaKpjS7cr3OhKoJs/giphy.gif'],
-        description: 'This product showcase app combines SVGs and the powerful GSAP animation library to bring together a narrative driven experience highlighting a full line of products with emphasis on home integration and IOT devices. All original artwork and images were created using Figma – the collaborative interface design tool.',
+        description: 'A product showcase app that combines SVGs and the powerful GSAP animation library to bring together a narrative driven experience highlighting a full line of products with emphasis on home integration and IOT devices. All original artwork and images were created using Figma – the collaborative interface design tool.',
         icons: ['react-logo', 'green-sock-logo', 'svg-logo']
     }
 ]
