239847f9de1368fd4d9324748a0adda50073f8e3
Adam Mathieson <mathieson1222@gmail.com>
2020-10-10 16:30:31 -0700

    figma logo animation, needs adjusting

--- js/projects/projects.js
+++ js/projects/projects.js
@@ -8,8 +8,8 @@ export const projects = [
             
         ],
         description: 'Looking to take on more responsibility? Water My Plants makes planning and caring for your home and office flora a breeze. This application uses Node backend support to send watering and care instruction reminders for all types of plants. A gorgeous user interface and image selection API will get your home screen looking like a page out of Home and Garden. Create healthy habits and you\'ll be ready for a dog in no time!',
-        // icons: ['svg-logo'],
-        icons: ['react-logo', 'redux-logo', 'nodejs-logo', 'material-ui-logo', 'moment-logo'],
+        icons: ['react-logo', 'green-sock-logo', 'svg-logo', 'figma-logo']
+        // icons: ['react-logo', 'redux-logo', 'nodejs-logo', 'material-ui-logo', 'moment-logo'],
     },
     {
         id: 'endrsd',   
@@ -21,6 +21,6 @@ export const projects = [
         id: 'home-devices',
         images: ['https://media.giphy.com/media/f4gaKpjS7cr3OhKoJs/giphy.gif'],
         description: 'A product showcase app that combines SVGs and the powerful GSAP animation library to bring together a narrative driven experience highlighting a full line of products with emphasis on home integration and IOT devices. All original artwork and images were created using Figma – the collaborative interface design tool.',
-        icons: ['react-logo', 'green-sock-logo', 'svg-logo']
+        icons: ['react-logo', 'green-sock-logo', 'svg-logo', 'figma-logo']
     }
 ]
