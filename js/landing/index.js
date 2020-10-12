a19926ff10f13bd19e4e3e37aac27494d98912b8
Adam Mathieson <mathieson1222@gmail.com>
2020-10-12 13:51:50 -0700

    moves the page transition logic to own file, animates on window resize but no longer reruns enitre page reveal but animates between mobile and full page curves – pretty sleek

--- js/landing/index.js
+++ js/landing/index.js
@@ -1,4 +1,5 @@
-import { tlLandMain, tlTitleReveal, setBackgroundHeight } from './landingAnimations.js'
+import './landingAnimations.js'
+import { pageTransition } from './pageTransition.js'
 // Replay animation on resize
 
 // window.resize event listener
@@ -6,11 +7,13 @@ var delay = 500
 var throttled = false
 window.addEventListener('resize', async function() {
     if (!throttled) {
-        setBackgroundHeight()
-        tlLandMain.reset()
-        tlLandMain.restart()
-
         throttled = false
+        pageTransition()
+        // tl_pageTransition.reset()
+        // tl_pageTransition.play()
+
+        // console.log(tl_pageTransition)
+
       // set a timeout to un-throttle
         setTimeout(function() {
             throttled = true       
@@ -25,10 +28,16 @@ window.addEventListener('resize', () => {
 })
 
 function doneResizing() {
-    setBackgroundHeight()
-    tlLandMain.reset()
-    tlLandMain.restart()
+    // tl_pageTransition.reset()
+    // tl_pageTransition.play()
+    pageTransition()
+    // tl_pageTransition.play()
+    // pageTransition()
+    // setBackgroundHeight()
+    // tlLandMain.reset()
+    // tlLandMain.play()
     // landingAnimations()
 }
-
+pageTransition()
+// tl_pageTransition.play()
 // let tlLanding = landingAnimations()
