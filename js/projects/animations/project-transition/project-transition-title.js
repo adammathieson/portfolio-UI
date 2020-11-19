aaec815fc099d92af7456730873298caf5e8cdc8
Adam Mathieson <mathieson1222@gmail.com>
2020-11-18 22:50:12 -0800

    adjusts the delay on stroke animation

--- js/projects/animations/project-transition/project-transition-title.js
+++ js/projects/animations/project-transition/project-transition-title.js
@@ -4,12 +4,12 @@ const projectStroke = anime({
     targets: '.project-title-path',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
-    duration: 3000,
-    delay: function(el, i) { return i * 250 },
+    // duration: 400,
+    delay: function(el, i) { return i * 500},
     // direction: 'alternate',
     // endDelay: 1000,
-    delay: 1000,
+    // delay: 500,
     // loop: true
 });
 
-// console.log(projectStroke)
+console.log(projectStroke.animations)
